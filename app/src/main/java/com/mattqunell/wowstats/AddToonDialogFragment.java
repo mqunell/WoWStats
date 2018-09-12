712957e7519e82880e8d8c5e2b198da856175172
Matt <mqunell@gmail.com>
2018-09-12 11:46:47 -0700

    Change add_realm's inputType

--- app/src/main/java/com/mattqunell/wowstats/AddToonDialogFragment.java
+++ app/src/main/java/com/mattqunell/wowstats/AddToonDialogFragment.java
@@ -5,12 +5,10 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 
-import com.mattqunell.wowstats.R;
 import com.mattqunell.wowstats.data.BattleNetConnection;
 
 /**
