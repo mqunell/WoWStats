6b2bb90bb03c51b03dff3bd5451a9261ec793364
Matt <mqunell@gmail.com>
2019-02-21 19:59:42 -0800

    Update demo_layout while changing customization

--- app/src/main/java/com/mattqunell/wowstats/CustomizeActivity.java
+++ app/src/main/java/com/mattqunell/wowstats/CustomizeActivity.java
@@ -2,14 +2,17 @@ package com.mattqunell.wowstats;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class CustomizeActivity extends AppCompatActivity {
 
@@ -59,6 +62,8 @@ public class CustomizeActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 mSharedPrefs.edit().putString(sharedPrefsKey, spinnerOptions[position]).apply();
+
+                setDemoTextview(sharedPrefsKey);
             }
 
             @Override
@@ -68,19 +73,74 @@ public class CustomizeActivity extends AppCompatActivity {
         });
     }
 
+    // Initializes a Switch
     private void initializeSwitch(Switch toggleColors) {
 
         // Set the Switch to display the current toggled status
         Boolean toggled = mSharedPrefs.getBoolean(ToonFormatter.FLIPPED_COLORS, false);
-
         toggleColors.setChecked(toggled);
+        setIconAndColor(toggled);
 
         // Listener; saves the toggled status to SharedPreferences
         toggleColors.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 mSharedPrefs.edit().putBoolean(ToonFormatter.FLIPPED_COLORS, isChecked).apply();
+
+                setIconAndColor(isChecked);
             }
         });
+    }
+
+    // Sets the appropriate TextView for the demo Toon to the SharedPrefs value
+    private void setDemoTextview(String sharedPrefsKey) {
+        TextView outputTextview;
+
+        switch (sharedPrefsKey) {
+            case ToonFormatter.TOP_LEFT_ONE:
+                outputTextview = findViewById(R.id.demo_tl1);
+                break;
+
+            case ToonFormatter.TOP_LEFT_TWO:
+                outputTextview = findViewById(R.id.demo_tl2);
+                break;
+
+            case ToonFormatter.TOP_RIGHT:
+                outputTextview = findViewById(R.id.demo_tr);
+                break;
+
+            case ToonFormatter.BOTTOM_LEFT:
+                outputTextview = findViewById(R.id.demo_bl);
+                break;
+
+            // case ToonFormatter.BOTTOM_RIGHT:
+            default:
+                outputTextview = findViewById(R.id.demo_br);
+                break;
+        }
+
+        // Replace "--Empty--" with ""
+        String outputText = mSharedPrefs.getString(sharedPrefsKey, "");
+        if (outputText.equals("--Empty--"))
+            outputText = "";
+
+        outputTextview.setText(outputText);
+    }
+
+    // Sets the icon and background color for the demo Toon based on toggle status
+    private void setIconAndColor(Boolean toggleStatus) {
+        if (!toggleStatus) {
+            ((ImageView) findViewById(R.id.demo_icon))
+                    .setImageDrawable(getResources().getDrawable(R.drawable.mage));
+            ((ConstraintLayout) findViewById(R.id.demo_layout))
+                    .setBackgroundColor(getResources().getColor(R.color.alliance));
+        }
+        else {
+            ((ImageView) findViewById(R.id.demo_icon))
+                    .setImageDrawable(getResources().getDrawable(R.drawable.alliance));
+            ((ConstraintLayout) findViewById(R.id.demo_layout))
+                    .setBackgroundColor(getResources().getColor(R.color.mage));
+
+        }
     }
 }
